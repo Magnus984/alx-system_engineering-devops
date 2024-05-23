406e9498c5fa3e6fceac483af7502f0ee39eafdd
Magnus <nharnharyhaw984@gmail.com>
2024-05-23 00:15:23 +0000

    Fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
-"""Gathers data from an API"""
+"""
+script that uses REST API
+to return TODO list progress for a given employee ID.
+"""
 import requests
 import sys
 
 
-employee_id = int(sys.argv[1])
-to_do_list = requests.get(
-        'https://jsonplaceholder.typicode.com/todos/'
-        ).json()
-completed = 0
-total = 0
-for i in range(len(to_do_list)):
-    if to_do_list[i].get("userId") == employee_id:
-        total += 1
-        if to_do_list[i].get("completed"):
-            completed += 1
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    to_do_list = requests.get(
+            'https://jsonplaceholder.typicode.com/todos/'
+            ).json()
+    completed = 0
+    total = 0
+    for i in range(len(to_do_list)):
+        if to_do_list[i].get("userId") == employee_id:
+            total += 1
+            if to_do_list[i].get("completed"):
+                completed += 1
 
-users = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-        ).json()
+    users = requests.get(
+            f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+            ).json()
 
-name = users.get('name')
-print(
-    f"Employee {name} is done with tasks({completed}/{total})"
-)
+    name = users.get('name')
+    print(
+        f"Employee {name} is done with tasks({completed}/{total})"
+    )
 
-for i in range(len(to_do_list)):
-    if to_do_list[i].get("userId") == employee_id:
-        if to_do_list[i].get("completed"):
-            print("\t {}".format(
-                to_do_list[i].get("title"))
-                )
+    for i in range(len(to_do_list)):
+        if to_do_list[i].get("userId") == employee_id:
+            if to_do_list[i].get("completed"):
+                print("\t {}".format(
+                    to_do_list[i].get("title"))
+                    )

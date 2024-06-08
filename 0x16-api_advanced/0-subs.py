2e1c24d2be9da5a97a9abbb7393f8f70c2b6b4e0
Magnus <nharnharyhaw984@gmail.com>
2024-06-08 06:49:12 +0000

    testing 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+            "User-Agent": "My Custom User-Agent"
+            }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    return 0
